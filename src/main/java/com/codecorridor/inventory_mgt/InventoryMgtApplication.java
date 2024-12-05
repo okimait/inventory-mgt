61b047fc6411bf810751f606a0e911e89b1c96a5
okimait <okimait@gmail.com>
2024-12-05 14:03:32 +0300

    item model added

---
+++ src/main/java/com/codecorridor/inventory_mgt/InventoryMgtApplication.java
@@ -0,0 +1,13 @@
+package com.codecorridor.inventory_mgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryMgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryMgtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecorridor/inventory_mgt/InventoryMgtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecorridor.inventory_mgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryMgtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

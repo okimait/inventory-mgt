e045084a032a4b0722704ee6ec9fb513871b5d5e
okimait <okimait@gmail.com>
2024-12-05 13:55:52 +0300

    item model added

---
+++ src/main/java/com/codecorridor/inventory_mgt/models/Item.java
@@ -0,0 +1,40 @@
+package com.codecorridor.inventory_mgt.models;
+
+import java.util.UUID;
+
+public class Item {
+
+    private UUID itemId;
+    private String name;
+    private short quantity;
+    private boolean isAvailable;
+
+    public Item(){}
+    public Item( String name, short quantity, boolean isAvailable){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public short getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(short quantity) {
+        this.quantity = quantity;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+}
